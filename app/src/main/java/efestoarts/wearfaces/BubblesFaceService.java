1f02aa1b7c397835594e8b4d26f644c75156131f
MarcoP <marco.polita@trovaprezzi.it>
2015-05-06 18:53:19 +0200

    Ambient mode

--- app/src/main/java/efestoarts/wearfaces/BubblesFaceService.java
+++ app/src/main/java/efestoarts/wearfaces/BubblesFaceService.java
@@ -3,6 +3,7 @@ package efestoarts.wearfaces;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -11,6 +12,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.text.format.Time;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 import java.util.concurrent.TimeUnit;
@@ -56,26 +58,35 @@ public class BubblesFaceService extends CanvasWatchFaceService {
             }
         };
 
-        Paint minutesBubblePaint;
-        Paint hoursBubblePaint;
-        Paint backgroundPaint;
+        private Paint minutesBubblePaint;
+        private Paint hoursBubblePaint;
+        private Paint backgroundPaint;
+        private Paint minutesBubblePaintAmbientMode;
+        private Paint backgroundPaintAmbientMode;
 
         @Override
         public void onCreate(SurfaceHolder holder) {
             super.onCreate(holder);
+
             minutesBubblePaint = new Paint();
-            minutesBubblePaint.setColor(getResources().getColor(R.color.minutes_bubble_color));
-            minutesBubblePaint.setStrokeCap(Paint.Cap.ROUND);
+            Resources resources = getResources();
+
+            minutesBubblePaint.setColor(resources.getColor(R.color.minutes_bubble_color));
             minutesBubblePaint.setAntiAlias(true);
 
             hoursBubblePaint = new Paint();
-            hoursBubblePaint.setColor(getResources().getColor(R.color.hours_bubble_color));
-            hoursBubblePaint.setStrokeCap(Paint.Cap.ROUND);
+            hoursBubblePaint.setColor(resources.getColor(R.color.hours_bubble_color));
             hoursBubblePaint.setAntiAlias(true);
+
+            minutesBubblePaintAmbientMode = new Paint();
+            minutesBubblePaintAmbientMode.setColor(resources.getColor(R.color.minutes_bubble_color_ambient));
 
 
             backgroundPaint = new Paint();
-            backgroundPaint.setColor(getResources().getColor(R.color.background_color));
+            backgroundPaint.setColor(resources.getColor(R.color.background_color));
+
+            backgroundPaintAmbientMode = new Paint();
+            backgroundPaintAmbientMode.setColor(resources.getColor(R.color.background_color_ambient));
 
             time = new Time();
         }
@@ -96,7 +107,7 @@ public class BubblesFaceService extends CanvasWatchFaceService {
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
-            /* the wearable switched between modes */
+            invalidate();
         }
 
         @Override
@@ -106,13 +117,19 @@ public class BubblesFaceService extends CanvasWatchFaceService {
             float hoursBubbleRadius = ((bounds.width() / 24) * time.hour) / 2;
             float minutesBubbleRadius = ((bounds.width() / 60) * time.minute) / 2;
 
-            canvas.drawPaint(backgroundPaint);
-            canvas.drawCircle(bounds.centerX(), bounds.centerY(), hoursBubbleRadius, hoursBubblePaint);
-            canvas.drawCircle(bounds.centerX(), bounds.centerY(), minutesBubbleRadius, minutesBubblePaint);
+            if (!isInAmbientMode())
+            {
+                canvas.drawPaint(backgroundPaint);
+                canvas.drawCircle(bounds.centerX(), bounds.centerY(), hoursBubbleRadius, hoursBubblePaint);
+                canvas.drawCircle(bounds.centerX(), bounds.centerY(), minutesBubbleRadius, minutesBubblePaint);
+            }
+            else
+            {
+                canvas.drawPaint(backgroundPaintAmbientMode);
+                canvas.drawCircle(bounds.centerX(), bounds.centerY(), minutesBubbleRadius, minutesBubblePaintAmbientMode);
 
+            }
 
-
-            //Qua ci deve essere una somma matematica dei colori calcolando l'area minima
         }
 
         @Override
